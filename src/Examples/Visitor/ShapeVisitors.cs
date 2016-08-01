c2631608600cdecbb3ba693fb683582c15e9edf0
James Swindell <swintwin@gmail.com>
2016-08-01 15:52:04 -0400

    initial

---
+++ src/Examples/Visitor/ShapeVisitors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examples.Visitor
+{
+    public interface IShapeVisitor
+    {
+        void Visit(Circle circle);
+        void Visit(Square square);
+        void Visit(Triangle triangle);
+    }
+
+    public class SimpleCounterVisitor : IShapeVisitor
+    {
+        public int CircleCount { get; private set; }
+        public int SquareCount { get; private set; }
+        public int TriangleCount { get; private set; }
+
+        public void Visit(Circle circle)
+        {
+            CircleCount++;
+        }
+
+        public void Visit(Square square)
+        {
+            SquareCount++;
+        }
+
+        public void Visit(Triangle triangle)
+        {
+            TriangleCount++;
+        }
+
+        public string SummarizeCounts()
+        {
+            return $"Here's the total count:\n\tCircles:\t{CircleCount}\n\tSquares:\t{SquareCount}\n\tTriangles:\t{TriangleCount}";
+        }
+    }
+}
---
+++ src/Examples/Visitor/Shapes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examples.Visitor
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public abstract void Accept(IShapeVisitor visitor);
+    }
+
+    public class Circle : Shape
+    {
+        public override void Accept(IShapeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Square : Shape
+    {
+        public override void Accept(IShapeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Accept(IShapeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
