70a5d6ed0b3f3d309b9d14b8a26483cafdfb7535
James Swindell <jswindell@users.noreply.github.com>
2016-08-11 12:40:46 -0400

    Correct misspelling of "randomizing" (#12)

--- src/Examples/Creational/Builder/BuilderDemo.cs
+++ src/Examples/Creational/Builder/BuilderDemo.cs
@@ -9,10 +9,10 @@ namespace Examples.Creational.Builder
     {
         public static void Run()
         {
-            Console.WriteLine("Create the 'Randomzing Demo Board'...");
+            Console.WriteLine("Create the 'Randomizing Demo Board'...");
 
             //Console.WriteLine("Initializing 'Simple Demo Board' with 50 shapes..."); -- NO LONGER NECESSARY !!!
-            IShapeBoardBuilder builder = RandomzingShapeBuilder.Create("Randomzing Demo Board", 50);
+            IShapeBoardBuilder builder = RandomizingShapeBuilder.Create("Randomizing Demo Board", 50);
             ShapeBoard board = builder.GetShapeBoard();
 
             Console.WriteLine($"Viewing shapes of '{board.Name}'...");
